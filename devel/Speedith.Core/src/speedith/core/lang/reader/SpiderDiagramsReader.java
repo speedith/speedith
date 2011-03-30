0730399d5f3f730b641b6c9d34127451c7853e1b
Matej Urbas <matej.urbas@gmail.com>
2011-03-30 18:35:12 +0100

    - Added the SpiderDiagramReader.

---
+++ devel/Speedith.Core/src/speedith/core/lang/reader/SpiderDiagramsReader.java
@@ -0,0 +1,41 @@
+/*
+ *   Project: Speedith.Core
+ * 
+ * File name: SpiderDiagramsReader.java
+ *    Author: Matej Urbas [matej.urbas@gmail.com]
+ * 
+ *  Copyright © 2010 Matej Urbas
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package speedith.core.lang.reader;
+
+/**
+ * This class provides the functionality of reading textual representations of
+ * spider diagrams and producing Java objects that represent the read spider
+ * diagrams.
+ * <p>The syntax of the textual representation of spider diagrams is specified
+ * in the 'SpiderDiagrams.g' file (which generates the {@link SpiderDiagramsParser parser}
+ * and the {@link SpiderDiagramsLexer lexer}).
+ * @author Matej Urbas [matej.urbas@gmail.com]
+ */
+public class SpiderDiagramsReader {
+
+}
