1b21ddf53d3f8e02d35b4b9774e21813134121c8
Matej Urbas <matej.urbas@gmail.com>
2012-03-30 10:11:06 -0700

    - Icon displays correctly.

--- devel/HyperSpeedith/src/speedith/openproof/editor/SpiderInternalEditor.java
+++ devel/HyperSpeedith/src/speedith/openproof/editor/SpiderInternalEditor.java
@@ -5,6 +5,12 @@ import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -16,42 +22,60 @@ import openproof.zen.repeditor.OPEEmbeddedEditorAdapter;
 import speedith.openproof.driver.SpiderDriver;
 
 public class SpiderInternalEditor extends OPEEmbeddedEditorAdapter {
+
 	public static final String REPRESENTATION_NAME = SpiderDriver.REPRESENTATION_NAME;
 	public static final Precondition[] PRECONDITIONS = null;
-	
 	private static SpiderExternalEditor extEditor = new SpiderExternalEditor();
-	
-	private static ImageIcon icns = new ImageIcon(Toolkit.getDefaultToolkit().getImage("SpeedithIconVennDiagram-32.png"));
-	
-	public SpiderInternalEditor() { }
-	
-	public Dimension getPreferredSize() { return new Dimension(32, 32); }
-	
-	public void displayIcon(boolean isAmbiguous) { }
-	
-	public String getRepresentationName() { return REPRESENTATION_NAME; }
-	
-	public void paint(Graphics g) {
-		g.drawImage(icns.getImage(), 0, 0, this);
+	private static BufferedImage icns;
+
+	static {
+		try {
+			InputStream imgStream = SpiderInternalEditor.class.getResourceAsStream("SpeedithIconVennDiagram-32.png");
+			icns = ImageIO.read(imgStream);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
-	public DiagrammaticRepresentationEditor getRepEditor() { return extEditor; }
-	
+
+	public SpiderInternalEditor() {
+	}
+
+	public Dimension getPreferredSize() {
+		return new Dimension(32, 32);
+	}
+
+	public void displayIcon(boolean isAmbiguous) {
+	}
+
+	public String getRepresentationName() {
+		return REPRESENTATION_NAME;
+	}
+
+	public void paint(Graphics g) {
+		g.drawImage(icns, 0, 0, this);
+	}
+
+	public DiagrammaticRepresentationEditor getRepEditor() {
+		return extEditor;
+	}
+
 	/**
-	 * This could be simplified by creating an action, but this would require Swing menu items
-	 * which we currently don't have.
+	 * This could be simplified by creating an action, but this would require
+	 * Swing menu items which we currently don't have.
 	 *
-	 * @see openproof.zen.repeditor.DiagrammaticRepresentationEditor#getDiagramMenuItems()
+	 * @see
+	 * openproof.zen.repeditor.DiagrammaticRepresentationEditor#getDiagramMenuItems()
 	 */
 	public Action[] getDiagramActions(final ActionListener al) {
 		Action actions[] = new Action[1];
-		
+
 		actions[0] = new AbstractAction() {
+
 			public void actionPerformed(ActionEvent e) {
 				al.actionPerformed(new NewSpiderDiagramActionEvent(e.getSource(), 0, e.getActionCommand()));
 			}
 		};
-		
+
 		actions[0].putValue(Action.SHORT_DESCRIPTION, "New Spider Diagram");
 
 		return actions;
