88d72d854c65a5dea3ec1eb23d4ba9a2bc3bf364
Matej Urbas <matej.urbas@gmail.com>
2012-04-12 13:45:11 +0100

    - Added the theory file where I will verify claims that I make during the construction of Diabelli.

---
+++ devel/Theories/Diabelli/HeterogeneousStatements.thy
@@ -0,0 +1,13 @@
+theory HeterogeneousStatements
+imports Main
+begin
+
+(* Lemma 1: If we have formula A' that is entailed by a premise A, then by
+  proving A' \<Longrightarrow> B, we also prove A \<Longrightarrow> B. *)
+lemma assumes en1: "A \<Longrightarrow> A'" and en2: "B' \<Longrightarrow> B" and new: "A' \<Longrightarrow> B'"
+      shows "A \<Longrightarrow> B"
+  by (fast intro: en1 en2 new)
+
+(* We will use diagrams *)
+
+end
