a9e57c541e3032dac8a610ecb7d74a6d1c5fed96
Matej Urbas <matej.urbas@gmail.com>
2012-04-17 23:46:53 +0100

    - Started with the implementation of the translation from Isabelle terms to spider diagrams.

--- devel/Theories/Diabelli/HeterogeneousStatements.thy
+++ devel/Theories/Diabelli/HeterogeneousStatements.thy
@@ -1,11 +1,23 @@
 theory HeterogeneousStatements
 imports Main
+uses
+  "GoalsExport.ML"
 begin
 
-(* Lemma 1: If we have formula A' that is entailed by a premise A, then by
-  proving A' \<Longrightarrow> B, we also prove A \<Longrightarrow> B. *)
+(* Lemma 1: If we have formula A' that is entailed by a premise A, and B'
+  entails the conclusion B, then by proving A' \<Longrightarrow> B', we also prove A \<Longrightarrow> B. *)
 lemma assumes en1: "A \<Longrightarrow> A'" and en2: "B' \<Longrightarrow> B" and new: "A' \<Longrightarrow> B'"
-      shows "A \<Longrightarrow> B"
-  by (fast intro: en1 en2 new)
+      shows old: "A \<Longrightarrow> B"
+proof -
+  assume a: "A"
+  show ?thesis using assms a
+    by (fast)
+qed
+
+(* Lemma 2: If we have a goal G to prove, and we know that G' \<Longrightarrow> G, can we
+    then prove G by just proving G'? *)
+lemma assumes en1: "G' \<Longrightarrow> G" and new: "G'"
+      shows "G"
+  by (fast intro: en1 new)
 
 end
