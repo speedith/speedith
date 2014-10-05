afc518b0ec62fe2c7a5c4e384ed2caab14f0cda0
Matej Urbas <matej.urbas@gmail.com>
2014-10-05 21:28:34 +0100

    Speedith now builds a distributable package.

---
+++ Speedith.Gui/src/main/executables/speedith-win32.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set JAVA_OPTS=-Djava.library.path=../win32-x86 -Djna.library.path=../win32-x86
+
+set TESSDATA_PREFIX=..
+
+speedith-runner.bat
---
+++ Speedith.Gui/src/main/executables/speedith-win64.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set JAVA_OPTS=-Djava.library.path=../win32-x86-64 -Djna.library.path=../win32-x86-64
+
+set TESSDATA_PREFIX=..
+
+speedith-runner.bat
