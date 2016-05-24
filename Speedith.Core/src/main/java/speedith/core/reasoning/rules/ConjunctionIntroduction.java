3a3a8b7e267507a0991b7aa195958aef74b4baf5
Sven Linker <s.linker@brighton.ac.uk>
2016-05-24 13:21:12 +0100

    added first version of conjunction introduction

--- Speedith.Core/src/main/java/speedith/core/reasoning/rules/ConjunctionIntroduction.java
+++ Speedith.Core/src/main/java/speedith/core/reasoning/rules/ConjunctionIntroduction.java
@@ -26,17 +26,16 @@
  */
 package speedith.core.reasoning.rules;
 
-import speedith.core.lang.DiagramType;
-import speedith.core.lang.IdTransformer;
-import speedith.core.lang.Transformer;
-import speedith.core.reasoning.ApplyStyle;
-import speedith.core.reasoning.RuleApplicationInstruction;
+import speedith.core.lang.*;
+import speedith.core.reasoning.*;
+import speedith.core.reasoning.args.RuleArg;
 import speedith.core.reasoning.args.SubDiagramIndexArg;
+import speedith.core.reasoning.rules.instructions.SelectSingleOperatorInstruction;
+import speedith.core.reasoning.rules.instructions.SelectSingleSubDiagramInstruction;
+import speedith.core.reasoning.rules.transformers.ConjunctionIntroductionTransformer;
 
 import java.io.Serializable;
-import java.util.EnumSet;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Matej Urbas [matej.urbas@gmail.com]
@@ -45,7 +44,7 @@ public class ConjunctionIntroduction extends UnaryForwardRule implements Seriali
 
     public static final String InferenceRuleName = "Conjunction Introduction";
 
-    private static final Set<DiagramType> applicableTypes = EnumSet.noneOf(DiagramType.class);
+    private static final Set<DiagramType> applicableTypes = EnumSet.of(DiagramType.SpiderDiagram, DiagramType.EulerDiagram);
     private static final long serialVersionUID = 5705804693584296643L;
 
     @Override
@@ -70,12 +69,33 @@ public class ConjunctionIntroduction extends UnaryForwardRule implements Seriali
 
     @Override
     public RuleApplicationInstruction<SubDiagramIndexArg> getInstructions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new SelectSingleOperatorInstruction(Operator.Conjunction);
     }
 
     @Override
     public Set<DiagramType> getApplicableTypes() {
         return applicableTypes;
+    }
+
+    @Override
+    protected RuleApplicationResult apply(final RuleArg args, Goals goals, ApplyStyle applyStyle) throws RuleApplicationException {
+        SubDiagramIndexArg arg = getTypedRuleArgs(args);
+        List<SpiderDiagram> newSubgoals = new LinkedList<>();
+        for (int i=0; i< goals.getGoalsCount();i++) {
+            if (i == arg.getSubgoalIndex()) {
+                SpiderDiagram target = getSubgoal(arg, goals);
+                newSubgoals.add(
+                        target.transform(new ConjunctionIntroductionTransformer(arg.getSubDiagramIndex(), applyStyle, 0)));
+                newSubgoals.add(
+                        target.transform(new ConjunctionIntroductionTransformer(arg.getSubDiagramIndex(), applyStyle, 1)));
+            } else {
+                newSubgoals.add(goals.getGoalAt(i));
+            }
+
+        }
+//        SpiderDiagram[] newSubgoals = goals.getGoals().toArray(new SpiderDiagram[goals.getGoalsCount()]);
+ //       newSubgoals[arg.getSubgoalIndex()] = getSubgoal(arg, goals).transform(getSententialTransformer(arg, applyStyle));
+        return createRuleApplicationResult(newSubgoals.toArray(new SpiderDiagram[goals.getGoalsCount()+1]));
     }
 
 }
---
+++ Speedith.Core/src/main/scala/speedith/core/reasoning/rules/transformers/ConjunctionIntroductionTransformer.scala
@@ -0,0 +1,39 @@
+package speedith.core.reasoning.rules.transformers
+
+
+
+import speedith.core.i18n.Translations._
+import speedith.core.lang.{TransformationException, SpiderDiagram, CompoundSpiderDiagram}
+import speedith.core.reasoning.ApplyStyle
+
+import speedith.core.reasoning.rules.SimpleInferenceRule
+import speedith.core.reasoning.rules.transformers.util.InferenceTargetChecks._
+
+
+/**
+  * TODO: Description
+  *
+  * @author Sven Linker [s.linker@brighton.ac.uk]
+  *
+  */
+class ConjunctionIntroductionTransformer(compundDiagramIndex : Int, applyStyle: ApplyStyle, operand: Int)
+  extends CompoundDiagramTransformer(compundDiagramIndex) {
+
+
+  override def transform(csd: CompoundSpiderDiagram, parents: java.util.ArrayList[CompoundSpiderDiagram], childIndices: java.util.ArrayList[Integer]): SpiderDiagram = {
+
+    assertIsConjunction(csd)
+
+
+    if (!SimpleInferenceRule.isAtFittingPosition(parents, childIndices, applyStyle, false)) {
+      if (applyStyle == ApplyStyle.Forward) {
+        throw new TransformationException(i18n("RULE_NOT_NEGATIVE_POSITION"))
+      } else {
+        throw new TransformationException(i18n("RULE_NOT_POSITIVE_POSITION"))
+      }
+    }
+
+    csd.getOperand(operand)
+  }
+
+}
